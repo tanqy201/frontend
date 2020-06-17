f8a2865b948c1c653c61f10d9c2670170bd587aa
Qiyang Tan <QiyangT>
2020-06-16 20:44:23 -0700

    react+redux

---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+class HomePage extends React.Component {
+    componentDidMount() {
+        this.props.getUsers();
+    }
+
+    handleDeleteUser(id) {
+        return (e) => this.props.deleteUser(id);
+    }
+
+    render() {
+        const { user, users } = this.props;
+        return (
+            <div>
+                <nav class="navbar navbar-inverse">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="#">
+                                <img src="/img/finhavenIcon.png" width="80" class="d-inline-block align-top" />
+                            </a>
+                        </div>
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="/">Home</a></li>
+                            <li><a href="/User">User</a></li>
+                            <li><a href="/Issuer">Issuer</a></li>
+                            <li><a href="/Security">Security</a></li>
+                            <li><a href="/Unknown">Unknown</a></li>
+                        </ul>
+                        <Link to="/login">
+                            <button class="btn btn-danger navbar-btn">Logout</button>
+                        </Link>
+                    </div>
+                </nav>
+                <div className="col-md-6 col-md-offset-3">
+                    <h1>Hi {user.firstName}!</h1>
+                    <p>You're logged in with React!!</p>
+                    <h3>All registered users:</h3>
+                    {users.loading && <em>Loading users...</em>}
+                    {users.error && <span className="text-danger">ERROR: {users.error}</span>}
+                    {users.items &&
+                        <ul>
+                            {users.items.map((user, index) =>
+                                <li key={user.id}>
+                                    {user.firstName + ' ' + user.lastName}
+                                    {
+                                        user.deleting ? <em> - Deleting...</em>
+                                            : user.deleteError ? <span className="text-danger"> - ERROR: {user.deleteError}</span>
+                                                : <span> - <a onClick={this.handleDeleteUser(user.id)}>Delete</a></span>
+                                    }
+                                </li>
+                            )}
+                        </ul>
+                    }
+                    <p>
+                        <Link to="/login">Logout</Link>
+                    </p>
+                </div>
+            </div>
+        );
+    }
+}
+
+function mapState(state) {
+    const { users, authentication } = state;
+    const { user } = authentication;
+    return { user, users };
+}
+
+const actionCreators = {
+    getUsers: userActions.getAll,
+    deleteUser: userActions.delete
+}
+
+const connectedHomePage = connect(mapState, actionCreators)(HomePage);
+export { connectedHomePage as HomePage };
---
+++ src/UserPage/UserPage.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import 'babel-polyfill';
+import { userActions } from '../_actions';
+
+// import {test} from '../fabric/network.js'
+// let network = require('../fabric/network.js');
+
+class UserPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            arg1: '',
+            data: [],
+            submitted: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    async handleSubmit(e) {
+        e.preventDefault();
+        this.setState({ submitted: true });
+        const { arg1 } = this.state;
+        const data = this.state.data;
+        if (arg1) {
+            this.setState({
+                data: data.concat([[data.length, arg1]])
+            }, ()=>{
+                this.updateTable();
+            });
+        }
+    }
+
+    async updateTable() {
+        const data = this.state.data;
+        console.log(data);
+        var table = document.getElementById("userTable");
+        var row = table.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell1.innerHTML = data[data.length-1][0];
+        cell2.innerHTML = data[data.length-1][1];
+    }
+
+    async test(info) {
+        alert("start getting");
+        return userActions.getAll();
+    }
+
+    render() {
+        // const { loggingIn } = this.props;
+        // const { username, password, submitted } = this.state;
+        const { arg1, submitted } = this.state;
+        return (
+            <div>
+                <nav class="navbar navbar-inverse">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="#">
+                                <img src="/img/finhavenIcon.png" width="80" class="d-inline-block align-top" />
+                            </a>
+                        </div>
+                        <ul class="nav navbar-nav">
+                            <li><a href="/">Home</a></li>
+                            <li class="active"><a href="/User">User</a></li>
+                            <li><a href="/Issuer">Issuer</a></li>
+                            <li><a href="/Security">Security</a></li>
+                            <li><a href="/Unknown">Unknown</a></li>
+                        </ul>
+                        <Link to="/login">
+                            <button class="btn btn-danger navbar-btn">Logout</button>
+                        </Link>
+                    </div>
+                </nav>
+                <div className="col-md-6 col-md-offset-3 leftBlock">
+                    <h2>Function</h2>
+                    <form name="form" onSubmit={this.handleSubmit}>
+                        <div className={'form-group' + (submitted && !arg1 ? ' has-error' : '')}>
+                            <label htmlFor="arg1">argument1</label>
+                            <input type="text" className="form-control" name="arg1" autoComplete="off" value={arg1} onChange={this.handleChange} />
+                            {submitted && !arg1 &&
+                                <div className="help-block">arg1 is required</div>
+                            }
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-primary">submit</button>
+                        </div>
+                    </form>
+                </div>
+                <div className="col-md-6 col-md-offset-3 rightBlock">
+                    <h2>Output</h2>
+                    <table id="userTable">
+                        <thead>
+                            <tr>
+                                <th>key</th>
+                                <th>value</th>
+                            </tr>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+// function mapState(state) {
+//     const { loggingIn } = state.authentication;
+//     return { loggingIn };
+// }
+
+// const actionCreators = {
+//     login: userActions.login,
+//     logout: userActions.logout
+// };
+
+// const connectedLoginPage = connect(mapState, actionCreators)(LoginPage);
+export { UserPage as UserPage };
